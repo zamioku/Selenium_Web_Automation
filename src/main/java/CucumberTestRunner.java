473409ce7cf2cb2357ee2415988129a362727361
Sorin Dragomir <zamioku@github.com>
2018-11-17 21:12:32 +0200

    Updated runner

--- src/main/java/CucumberTestRunner.java
+++ src/main/java/CucumberTestRunner.java
@@ -1,8 +1,14 @@
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+		features="src/test/resources/Features", 
+//		glue = "src/test/java/StepDefinitions", 
+		tags={"@tag1"}
+		)
 public class CucumberTestRunner {
 
 }
