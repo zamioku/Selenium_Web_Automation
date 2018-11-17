850f302d47a7510cbd8dd2b23962e5bde5ebef73
Sorin Dragomir <zamioku@github.com>
2018-11-17 20:37:39 +0200

    Added Cucumber dependencies

---
+++ src/test/java/StepDefinitions/FirstSteps.java
@@ -0,0 +1,75 @@
+package StepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class FirstSteps {
+
+	@Given("I want to write a step with precondition")
+	public void i_want_to_write_a_step_with_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("some other precondition")
+	public void some_other_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("I complete action")
+	public void i_complete_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("some other action")
+	public void some_other_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("yet another action")
+	public void yet_another_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("I validate the outcomes")
+	public void i_validate_the_outcomes() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("check more outcomes")
+	public void check_more_outcomes() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("I want to write a step with name{int}")
+	public void i_want_to_write_a_step_with_name(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("I check for the {int} in step")
+	public void i_check_for_the_in_step(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("I verify the success in step")
+	public void i_verify_the_success_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("I verify the Fail in step")
+	public void i_verify_the_Fail_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+	
+}
